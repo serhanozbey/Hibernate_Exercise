ecd8797e3d6b7ec596ef1774170c788184b0e576
serhanozbey <ozbey.serhan@gmail.com>
2018-05-01 02:18:02 +0200

    1.1 Adding uniqueness to username, autoincrement strategy to ID, small change to xml's

---
+++ src/main/java/com/serhan/hibernate/HibernateTest.java
@@ -0,0 +1,35 @@
+package com.serhan.hibernate;
+
+import com.serhan.hibernate.model.UserDetails;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class HibernateTest {
+    
+    public static void main(String[] args) {
+        UserDetails user1 = new UserDetails();
+        user1.setUsername("serhanozb");
+        UserDetails user2 = new UserDetails();
+        user2.setUsername("yunus");
+        UserDetails user3 = new UserDetails();
+        user3.setUsername("deniz");
+        //we can use factory method to have new sessions to do operations.
+        SessionFactory sessionFactory = new Configuration().configure("com/serhan/hibernate/hibernate.cfg.xml").buildSessionFactory();
+        //we will use session objects to save our model objects.
+        Session session = sessionFactory.openSession();
+        //we will use this transaction to perform a save
+        session.beginTransaction();
+        //saving object
+        session.save(user1);
+        session.save(user2);
+        session.save(user3);
+        //committing the transaction
+        session.getTransaction().commit();
+        
+        //Hibernate is gonna create the table for us.
+        
+    }
+    
+}
---
+++ src/main/java/com/serhan/hibernate/model/UserDetails.java
@@ -0,0 +1,59 @@
+package com.serhan.hibernate.model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_details",uniqueConstraints = {
+        @UniqueConstraint( name = "const_username",columnNames = "username")})
+public class UserDetails {
+    
+    //1- This is the model class that Hibernate needs to persist*/
+    
+    //2- Now we will use annotations to give Hibernate clue*/
+    
+    //3- Private key assignment
+    
+    //4- We need an object initialization code.
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private int id;
+    private String username;
+    @NotNull
+    private String password;
+    @NotNull
+    private String email;
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
